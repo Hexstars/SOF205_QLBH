f9e135b4ec9f14e9940a6142cbbf627a419f76a9
Eskhaton <bao90531@gmail.com>
2024-06-13 13:26:50 +0700

    Sửa GUI_NhanVien

--- _1_GUI_QLBH/GUI_NhanVien.cs
+++ _1_GUI_QLBH/GUI_NhanVien.cs
@@ -13,7 +13,58 @@ namespace _1_GUI_QLBH
 {
     public class GUI_NhanVien
     {
-        
+        public string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            if (lowerCase)
+            {
+                return builder.ToString().ToLower();
+            }
+            return builder.ToString();
+        }
+
+        //Tao so ngau nhien
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        public void SendMail(string email, string maxacnhan)
+        {
+            try
+            {
+                //Now we must create a new Smtp client to send our email.
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //smtp.gmail.com // For Gmail //Authentication.
+                                                                           //This is where the valid email account comes into play. You must have a valid email //account (with password) to give our program a place to send the mail from.
+                NetworkCredential cred = new NetworkCredential("baotncps40789@gmail.com", "jflb jhdg dqht lwra");
+                //To send an email we must first create a new mailMessage (an email) to send. 
+                MailMessage Msg = new MailMessage();
+                // Sender e-mail address.
+                Msg.From = new MailAddress("baotncps40789@gmail.com"); //Nothing But Above Credentials or your credentials (*******@gmail.com) // Recipient e-mail address.
+                Msg.To.Add(email);
+                // Assign the subject of our message.
+                Msg.Subject = "Anh/chị đã sử dụng tính năng quên mật khẩu";
+                // Create the content (body) of our message.
+                Msg.Body = "Chào anh/chị. Mã xác nhận là: " + maxacnhan;
+                // Send our account login details to the client.
+                client.Credentials = cred;
+                //Enabling SSL (Secure Sockets Layer, encyription) is reqiured by most email providers to send mail
+                client.EnableSsl = true;
+                client.Send(Msg); // Send our email.
+            }
+            catch (Exception)
+            {
+            }
+        }
     }
     
 }
--- _1_GUI_QLBH/frmCapNhatMK.cs
+++ _1_GUI_QLBH/frmCapNhatMK.cs
@@ -25,58 +25,6 @@ namespace _1_GUI_QLBH
         private void frmCapNhatMK_Load(object sender, EventArgs e)
         {
         }
-        public string RandomString(int size, bool lowerCase)
-        {
-            StringBuilder builder = new StringBuilder();
-            Random random = new Random();
-            char ch;
-            for (int i = 0; i < size; i++)
-            {
-                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
-                builder.Append(ch);
-            }
-
-            if (lowerCase)
-            {
-                return builder.ToString().ToLower();
-            }
-            return builder.ToString();
-        }
-
-        //Tao so ngau nhien
-        public int RandomNumber(int min, int max)
-        {
-            Random random = new Random();
-            return random.Next(min, max);
-        }
-
-        public void SendMail(string email, string maxacnhan)
-        {
-            try
-            {
-                //Now we must create a new Smtp client to send our email.
-                SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //smtp.gmail.com // For Gmail //Authentication.
-                                                                           //This is where the valid email account comes into play. You must have a valid email //account (with password) to give our program a place to send the mail from.
-                NetworkCredential cred = new NetworkCredential("baotncps40789@gmail.com", "jflb jhdg dqht lwra");
-                //To send an email we must first create a new mailMessage (an email) to send. 
-                MailMessage Msg = new MailMessage();
-                // Sender e-mail address.
-                Msg.From = new MailAddress("baotncps40789@gmail.com"); //Nothing But Above Credentials or your credentials (*******@gmail.com) // Recipient e-mail address.
-                Msg.To.Add(email);
-                // Assign the subject of our message.
-                Msg.Subject = "Anh/chị đã sử dụng tính năng quên mật khẩu";
-                // Create the content (body) of our message.
-                Msg.Body = "Chào anh/chị. Mã xác nhận là: " + maxacnhan;
-                // Send our account login details to the client.
-                client.Credentials = cred;
-                //Enabling SSL (Secure Sockets Layer, encyription) is reqiured by most email providers to send mail
-                client.EnableSsl = true;
-                client.Send(Msg); // Send our email.
-            }
-            catch (Exception)
-            {
-            }
-        }
         private void btnCapNhat_Click(object sender, EventArgs e)
         {
             if (txtEmail.Text.Trim().Length == 0)
@@ -150,6 +98,7 @@ namespace _1_GUI_QLBH
         BUS_NhanVien busNV = new BUS_NhanVien();
 
         string maXacNhan;
+        GUI_NhanVien guiNV = new GUI_NhanVien();
         private void btnGuiMa_Click(object sender, EventArgs e)
         {
             if (txtEmail.Text != "")
@@ -157,13 +106,13 @@ namespace _1_GUI_QLBH
                 if (busNV.NhanVienQuenMK(txtEmail.Text))
                 {
                     StringBuilder builder = new StringBuilder();
-                    builder.Append(RandomString(4, true));
-                    builder.Append(RandomNumber(1000, 9999));
-                    builder.Append(RandomString(2, false));
+                    builder.Append(guiNV.RandomString(4, true));
+                    builder.Append(guiNV.RandomNumber(1000, 9999));
+                    builder.Append(guiNV.RandomString(2, false));
 
                     maXacNhan = builder.ToString();
 
-                    SendMail(txtEmail.Text, maXacNhan);
+                    guiNV.SendMail(txtEmail.Text, maXacNhan);
 
                     MessageBox.Show($"Mã xác nhận đã được gửi về {txtEmail.Text}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     txtMaXN.Focus();
